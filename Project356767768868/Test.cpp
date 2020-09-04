c765aa7479027ec879c7333eb830214adf0c8144
Félix-Antoine <2064251@cstj.qc.ca>
2020-09-04 13:19:25 -0400

    Premier commit/validation du projet avec git

--- Project356767768868/Test.cpp
+++ Project356767768868/Test.cpp
@@ -2,3 +2,13 @@
 // ce programme avec git et de l'héberger sur la plateforme GitHub.
 // Auteur: Félix-Antoine Campbell
 // Date: 2020-09-04
+
+
+int main(/*Liste des paramètres séparés par une virgule le cas échéant*/)
+{
+
+
+
+
+	return 0;
+}
